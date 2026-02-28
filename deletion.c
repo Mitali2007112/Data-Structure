270a32f13a2ef3d9980312fbf005c81cc6f6c57c
Mitali2007112 <mitalisitapara123@gmail.com>
2026-02-28 13:41:36 +0530

    Add deletion.c for array element insertion

---
+++ deletion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+ 
+	int a[10],i,pos,element,n=8;
+	printf("enter the element in array");
+	for(int i=0; i<n; i++){
+	
+		scanf("%d",&a[i]);
+	}
+		printf("Enter position");
+	scanf("%d",&pos);
+	    printf("enter element");
+	scanf("%d",&element);
+	for (i=n;i>pos-1;i++){
+	  a[i]=a[i+1];
+	}
+	a[pos]=element;
+	n++;
+	printf("Array after deletion");
+	
+	for(i=0;i<10;i++)
+	{
+		printf("%d",a[i]);
+	}
+	return 0;
+}   
+    
